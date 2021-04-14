a91648478406c87d686c67a964108b17f7889143
yanglh14 <yanglh14@163.com>
2021-04-14 15:09:55 +0800

    add Onrobot FTsensor driver

---
+++ FTSensor/build/CMakeFiles/FTReading.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yanglh/Desktop/OpticalFiber/FTReading/src/Force.cpp" "/home/yanglh/Desktop/OpticalFiber/FTReading/build/CMakeFiles/FTReading.dir/src/Force.cpp.o"
+  "/home/yanglh/Desktop/OpticalFiber/FTReading/src/pyFT.cpp" "/home/yanglh/Desktop/OpticalFiber/FTReading/build/CMakeFiles/FTReading.dir/src/pyFT.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/yanglh/anaconda3/include/python3.8"
+  "/home/yanglh/anaconda3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FTSensor/build/CMakeFiles/FTReading.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FTReading.dir/src/Force.cpp.o"
+  "CMakeFiles/FTReading.dir/src/pyFT.cpp.o"
+  "FTReading.pdb"
+  "FTReading.cpython-38-x86_64-linux-gnu.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FTReading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
